719a084c8f4768010137b127e10e9cb5acb15847
yasharzb <yasharzb@mail.ce.sharif.edu>
2019-09-15 18:22:16 +0430

    EstateInquiryConfig sample is implemented.

---
+++ src/main/java/ir/sapp/bot/municipality/configuration/constructional/EstateInquiryConfig.java
@@ -0,0 +1,29 @@
+package ir.sapp.bot.municipality.configuration.constructional;
+
+import ir.sapp.bot.municipality.configuration.APIConfig.ConstructionalConfig;
+import ir.sapp.bot.municipality.model.services.constructional.EstateInquiry.EstateInquiryRequestProcessor;
+import ir.sapp.bot.municipality.model.services.constructional.EstateInquiry.EstateInquiryResponseProcessor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EstateInquiryConfig extends RouteBuilder {
+
+    @Autowired
+    private ConstructionalConfig constructionalConfig;
+
+    @Autowired
+    private EstateInquiryRequestProcessor estateInquiryRequestProcessor;
+
+    @Autowired
+    private EstateInquiryResponseProcessor estateInquiryResponseProcessor;
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:" + constructionalConfig.getFaultApi()).process(estateInquiryRequestProcessor).
+                to("http:" + constructionalConfig.getFaultApi());
+        from("http:" + constructionalConfig.getFaultApi()).process(estateInquiryResponseProcessor).
+                to("soroush:sendMessage");
+    }
+}

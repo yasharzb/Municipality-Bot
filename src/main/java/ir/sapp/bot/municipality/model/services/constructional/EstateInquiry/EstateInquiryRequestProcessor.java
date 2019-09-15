5bca6ac4067518a8a51bd95d845d0e01de4a3b75
yasharzb <yasharzb@mail.ce.sharif.edu>
2019-09-15 18:22:48 +0430

    EstateInquiry processors are autowired.

---
+++ src/main/java/ir/sapp/bot/municipality/model/services/constructional/EstateInquiry/EstateInquiryRequestProcessor.java
@@ -0,0 +1,13 @@
+package ir.sapp.bot.municipality.model.services.constructional.EstateInquiry;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstateInquiryRequestProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+    }
+}
---
+++ src/main/java/ir/sapp/bot/municipality/model/services/constructional/EstateInquiry/EstateInquiryResponseProcessor.java
@@ -0,0 +1,14 @@
+package ir.sapp.bot.municipality.model.services.constructional.EstateInquiry;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstateInquiryResponseProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+    }
+}

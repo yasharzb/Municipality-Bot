9bc52ab6f1c7758851ce013743b0c7080fefcf53
yasharzb <yasharzb@mail.ce.sharif.edu>
2019-09-15 09:58:32 +0430

    Initial commit

---
+++ src/main/java/ir/sapp/bot/municipality/MunicipalityApplication.java
@@ -0,0 +1,13 @@
+package ir.sapp.bot.municipality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunicipalityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/sapp/bot/municipality/MunicipalityApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.sapp.bot.municipality;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MunicipalityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
